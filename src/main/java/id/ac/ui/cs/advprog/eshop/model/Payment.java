9d1dcbfa3ec490b70f5491becb4eeb38199bbeab
angelojohenry <johenry.angelo@gmail.com>
2026-03-10 20:14:53 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -16,6 +16,11 @@ public class Payment {
     private Order order;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "PENDING";
     }
 
 }
