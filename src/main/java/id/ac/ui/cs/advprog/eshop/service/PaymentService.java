7ffd010e0fffd1cc5d41229b7468324c5f4f6bdd
angelojohenry <johenry.angelo@gmail.com>
2026-03-10 20:57:30 +0700

    [GREEN] Implement Payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -17,11 +17,32 @@ public class PaymentService {
     private PaymentRepository paymentRepository;
 
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+
+        String id = UUID.randomUUID().toString();
+        Payment payment = new Payment(id, order, method, paymentData);
+
+        if (method.equals("VOUCHER")) {
+            validateVoucher(payment, paymentData);
+        }
+        if (method.equals("COD")) {
+            validateCOD(payment, paymentData);
+        }
+        return paymentRepository.save(payment);
     }
 
     public Payment setStatus(Payment payment, String status) {
-        return null;
+
+        payment.setStatus(status);
+
+        if (status.equals("SUCCESS")) {
+            payment.getOrder().setStatus("SUCCESS");
+        }
+
+        if (status.equals("REJECTED")) {
+            payment.getOrder().setStatus("FAILED");
+        }
+
+        return payment;
     }
 
     public Payment getPayment(String paymentId) {
@@ -34,9 +55,41 @@ public class PaymentService {
 
     private void validateVoucher(Payment payment, Map<String,String> data) {
 
+        String voucher = data.get("voucherCode");
+
+        if(voucher == null) {
+            payment.setStatus("REJECTED");
+            return;
+        }
+
+        boolean validLength = voucher.length() == 16;
+        boolean validPrefix = voucher.startsWith("ESHOP");
+
+        int digitCount = 0;
+        for(char c : voucher.toCharArray()) {
+            if(Character.isDigit(c)) {
+                digitCount++;
+            }
+        }
+
+        if(validLength && validPrefix && digitCount == 8) {
+            payment.setStatus("SUCCESS");
+        } else {
+            payment.setStatus("REJECTED");
+        }
     }
 
     private void validateCOD(Payment payment, Map<String,String> data) {
 
+        String address = data.get("address");
+        String deliveryFee = data.get("deliveryFee");
+
+        if(address == null || address.isEmpty()
+                || deliveryFee == null || deliveryFee.isEmpty()) {
+
+            payment.setStatus("REJECTED");
+        } else {
+            payment.setStatus("SUCCESS");
+        }
     }
 }
